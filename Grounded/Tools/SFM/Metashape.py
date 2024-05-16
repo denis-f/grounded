f1f8563d2623ba2fac3dd202b6c92ac17937afd3
Norman <norman.francois@supagro.fr>
2024-05-16 14:48:56 +0200

    feat : Ajout de la récupérations des photo et de leur alignement dans le module Metashape SFM

--- Grounded/Tools/SFM/Metashape.py
+++ Grounded/Tools/SFM/Metashape.py
@@ -3,6 +3,13 @@ from ...DataObject import Image, Mire3D, PointCloud
 
 import Metashape as ms
 import os
+import re
+
+
+def add_photo_with_prefix(chunk: ms.Chunk, photos: list[str], prefix: str):
+    chunk.addPhotos(photos)
+    for camera in chunk.cameras:
+        camera.label = f"{prefix}{camera.label}"
 
 
 class Metashape(SFM):
@@ -11,17 +18,32 @@ class Metashape(SFM):
         self.doc = ms.Document()  # création d'un projet
         self.chunk = self.doc.addChunk()  # ajout d'un chunk dans lequel nous allons travailler
 
-    def detection_points_homologues(self, chemin_dossier_avant: str, chemin_dossier_apres: str):
+    def _add_photos_to_chunk(self, chemin_dossier_avant: str, chemin_dossier_apres: str):
+        # Ajout des photos avant excavation avec le préfixe "0_"
         photos = [os.path.join(chemin_dossier_avant, image_name) for image_name in os.listdir(chemin_dossier_avant)]
-        photos += [os.path.join(chemin_dossier_apres, image_name) for image_name in os.listdir(chemin_dossier_apres)]
-        self.chunk.addPhotos(photos)
-        self.chunk.matchPhotos()
+        chunk_before = self.doc.addChunk()
+        add_photo_with_prefix(chunk_before, photos, "0_")
 
-    def calibration(self):
+        # Ajout des photos avant excavation avec le préfixe "1_"
+        photos = [os.path.join(chemin_dossier_apres, image_name) for image_name in os.listdir(chemin_dossier_apres)]
+        chunk_after = self.doc.addChunk()
+        add_photo_with_prefix(chunk_after, photos, "1_")
+
+        self.chunk = self.doc.mergeChunks(chunk_before, chunk_after)
+
+    def _align_images(self):
+        self.chunk.matchPhotos(downscale=1, generic_preselection=True, reference_preselection=True,
+                               reference_preselection_mode=ms.ReferencePreselectionSource, keypoint_limit=100000,
+                               tiepoint_limit=10000, keep_keypoints=True)
         self.chunk.alignCameras()
+        self.chunk.resetRegion()
+        self.chunk.optimizeCameras()
+        self.chunk.updateTransform()
 
-    def generer_nuages_de_points(self) -> tuple[PointCloud, PointCloud]:
-        pass
+    def generer_nuages_de_points(self, chemin_dossier_avant: str, chemin_dossier_apres: str) -> tuple[PointCloud, PointCloud]:
+        self._add_photos_to_chunk(chemin_dossier_avant, chemin_dossier_apres)
+        self._align_images()
+
 
     def calculer_coordinates_3d_mires(self, image: Image) -> list[Mire3D]:
         pass
