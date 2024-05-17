3776bbc2418fd49f67f05294d3ad8a57e5849616
Norman <norman.francois@supagro.fr>
2024-05-17 11:43:18 +0200

    feat : Ajout de la récupération des coordonnées 3d à partir des coordonnées 2d d'une image

--- Grounded/Tools/SFM/Metashape.py
+++ Grounded/Tools/SFM/Metashape.py
@@ -1,5 +1,5 @@
 from .SFM import SFM
-from ...DataObject import Image, Mire3D, PointCloud
+from Grounded.DataObject import Image, Mire3D, PointCloud
 
 import Metashape as ms
 import os
@@ -21,11 +21,45 @@ def copy_chunk_with_camera_with_prefix(chunk: ms.Chunk, prefix: str) -> ms.Chunk
     return copy_chunk
 
 
+def build_point_cloud(chunk: ms.Chunk, path: str, downscale: int) -> PointCloud:
+    chunk.buildDepthMaps(downscale=downscale, filter_mode=ms.AggressiveFiltering)
+    chunk.buildPointCloud(point_colors=True)
+    chunk.exportPointCloud(path=path, format=ms.PointCloudFormatPLY, clip_to_boundary=True)
+    return PointCloud(path)
+
+
+def photos_coord_to_3d_coords(chunk: ms.Chunk, x:float, y:float, camera_name: str) -> tuple[float, float, float]:
+    cameras = [camera for camera in chunk.cameras if
+               camera.transform and camera.type == ms.Camera.Type.Regular]  # list of aligned cameras
+
+    surface = chunk.point_cloud
+    crs = chunk.crs
+    transform_matrix = chunk.transform.matrix
+
+    for camera in cameras:
+        if camera.label == camera_name:
+            marker = chunk.addMarker()
+            ray_origin = camera.unproject(ms.Vector([x, y, 0]))
+            ray_target = camera.unproject(ms.Vector([x, y, 1]))
+            coord = crs.project(transform_matrix.mulp(surface.pickPoint(ray_origin, ray_target)))
+            marker.label = camera_name
+            marker.projections[camera] = ms.Marker.Projection(ms.Vector([x, y]), True)
+            marker.reference.location = coord
+            marker.reference.enabled = True
+            return coord.x, coord.y, coord.z
+
+    raise ValueError("Aucune camera alignée ne porte ce nom")
+
+
 class Metashape(SFM):
 
-    def __init__(self):
+    def __init__(self, downscale="8"):
         self.doc = ms.Document()  # création d'un projet
         self.chunk = self.doc.addChunk()  # ajout d'un chunk dans lequel nous allons travailler
+        self.working_directory = "metashape_working_directory"
+        self.downscale = int(downscale)
+        self.chunk_before = None
+        self.chunk_after = None
 
     def _add_photos_to_chunk(self, chemin_dossier_avant: str, chemin_dossier_apres: str):
         # Ajout des photos avant excavation avec le préfixe "0_"
@@ -50,25 +84,37 @@ class Metashape(SFM):
         self.chunk.optimizeCameras()
         self.chunk.updateTransform()
 
-    def generer_nuages_de_points(self, chemin_dossier_avant: str, chemin_dossier_apres: str) -> tuple[
-        PointCloud, PointCloud]:
+    def generer_nuages_de_points(self, chemin_dossier_avant: str, chemin_dossier_apres: str) -> tuple[PointCloud, PointCloud]:
         self._add_photos_to_chunk(chemin_dossier_avant, chemin_dossier_apres)
         self._align_images()
 
-        chunk_before = copy_chunk_with_camera_with_prefix(self.chunk, "0_")
-        chunk_before.buildDepthMaps(downscale=8, filter_mode=ms.AggressiveFiltering,)
-        chunk_before.buildPointCloud(point_colors=True)
-        chunk_before.exportPointCloud(path="/home/lisahcalc/Grounded/avant_cloud(2).ply",
-                                      format=ms.PointCloudFormatPLY, clip_to_boundary=True)
+        self.chunk_before = copy_chunk_with_camera_with_prefix(self.chunk, "0_")
+        point_cloud_before = build_point_cloud(self.chunk_before,
+                                               os.path.join(self.working_directory, "point_cloud_0.ply"),
+                                               self.downscale)
 
-        chunk_after = copy_chunk_with_camera_with_prefix(self.chunk, "1_")
-        chunk_after.buildDepthMaps(downscale=8, filter_mode=ms.AggressiveFiltering)
-        chunk_after.buildPointCloud(point_colors=True)
-        chunk_after.exportPointCloud(path="/home/lisahcalc/Grounded/apres_cloud(2).ply",
-                                      format=ms.PointCloudFormatPLY, clip_to_boundary=True)
+        self.chunk_after = copy_chunk_with_camera_with_prefix(self.chunk, "1_")
+        point_cloud_after = build_point_cloud(self.chunk_after,
+                                              os.path.join(self.working_directory, "point_cloud_1.ply"),
+                                              self.downscale)
+
+        return point_cloud_before, point_cloud_after
 
     def calculer_coordinates_3d_mires(self, image: Image) -> list[Mire3D]:
-        pass
+        if len([camera for camera in self.chunk_before.cameras
+                if re.search(f"0_{image.get_name_without_extension()}", camera.label) is not None]) > 0:
+            chunk = self.chunk_before
+            local_name = f"0_{image.get_name_without_extension()}"
+        else:
+            chunk = self.chunk_after
+            local_name = f"1_{image.get_name_without_extension()}"
+
+        mires3d = []
+        for mire2d in image.mires_visibles:
+            x, y, z = photos_coord_to_3d_coords(chunk, mire2d.coordinates[0], mire2d.coordinates[1], local_name)
+            mires3d.append(Mire3D(mire2d.identifier, (x, y, z)))
+
+        return mires3d
 
     def get_config(self) -> str:
-        pass
+        return f"Metashape(working_directory={self.working_directory}, downscale={self.downscale})"
