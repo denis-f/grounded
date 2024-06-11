8fed9124e22883967a0f0fb2de43c7ff5ecfd442
Denis Feurer <denis.feurer@ird.fr>
2024-06-11 18:19:11 +0200

    test sur les mires pour savoir si ce sont les bonnes scalebar qui ont été chargées

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from Grounded.Tools.DetecteurMire.DetectionCCTag import DetectionCCTag
+from Grounded.Tools.DetecteurMire.DetectionMetashape import DetectionMetashape
 from Grounded.Tools.SFM import MicMac
 from Grounded.Tools.PointCloudProcessor import CloudCompare
 from Grounded.DensityAnalyser import DensityAnalyser
 from Grounded.ScaleBarLoader import ScaleBarLoader
 from Grounded.Tools.SFM.Metashape import Metashape
 
-scale_bars = ScaleBarLoader.load("Configuration/scaleBar.csv")
+scale_bars = ScaleBarLoader.load("Configuration/oldScaleBar.csv")
 
 
 #m = MicMac("/opt/micmac/bin/mm3d", "FraserBasic", "QuickMac")  # initialisation d'un SFM
 m = Metashape(8)
-c = CloudCompare("cloudcompare.CloudCompare", "2.11.1")  # initialisation de PointCloudProcessor
-d = DetectionCCTag("/opt/CCTag/")  # initialisation d'un DetecteurMire
+c = CloudCompare("'C:\Program Files\CloudCompare\CloudCompare.exe'", "2.13.1")  # initialisation de PointCloudProcessor
+d = DetectionMetashape()  # initialisation d'un DetecteurMire
 
 # instantiation d'un objet DensityAnalyser construit à partir des modules défini au-dessus
 analyser = DensityAnalyser(m, d, c)
 # analyse du volume des trous présents sur les photos
-volumes_trous = analyser.analyse("..\\..\\Dossier_photo_test\\avant", # à compléter par le dossier contenant les images avant excavation
-                                 "..\\..\\Dossier_photo_test\\apres",  # à compléter par le dossier contenant les images après excavation
+volumes_trous = analyser.analyse("..\\avant", # à compléter par le dossier contenant les images avant excavation
+                                 "..\\apres",  # à compléter par le dossier contenant les images après excavation
                                  scale_bars)
 
 print("###########################################################################\n"
